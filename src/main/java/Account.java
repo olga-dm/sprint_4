9ec9d521df14c04d5bf0fe902270ab39b09c9250
Olga Dmitrieva <fialkovskai@gmail.com>
2022-05-31 18:48:24 +0300

    added regex

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,19 +1,18 @@
-public class Account {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class Account {
     private final String name;
+    private final Pattern namePattern = Pattern.compile("((?=(^[а-яА-Я-]+\\s[а-яА-Я-]+$))(?=(^.{3,19}$)))", Pattern.COMMENTS);
 
     public Account(String name) {
         this.name = name;
     }
 
     public boolean checkNameToEmboss() {
-        if (name != null && name.length() >= 3 &&
-                name.length() <= 19 &&
-                name.contains(" ") &&
-                !name.startsWith(" ") &&
-                !name.endsWith(" ")) {
-            return true;
-        }
-        return false;
+        if (name == null || name.isEmpty())
+            return false;
+        Matcher matcher = namePattern.matcher(name);
+        return matcher.find();
     }
 }
